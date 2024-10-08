1c575fe28654acddbb94adad7eb654fda83e8a11
Jack Caesar <jackachulian@gmail.com>
2024-10-08 18:51:46 -0500

    Cleaner looking slash colors, may implement texture eventually

--- Assets/Scripts/SlashEffect.gd
+++ Assets/Scripts/SlashEffect.gd
@@ -1,7 +1,7 @@
 class_name SlashEffect
 extends Node3D
 
-@export var duration: float = 0.5
+@export var duration: float = 0.15
 @export var mesh: CSGPolygon3D
 	
 func animate():
@@ -9,10 +9,6 @@ func animate():
 	mesh.material.set_shader_parameter("t", 0)
 	await get_tree().create_timer(duration * 0.666).timeout
 	mesh.material.set_shader_parameter("t", 0.5)
-	await get_tree().create_timer(duration * 0.111).timeout
-	mesh.material.set_shader_parameter("t", 0.75)
-	await get_tree().create_timer(duration * 0.111).timeout
-	mesh.material.set_shader_parameter("t", 0.875)
-	await get_tree().create_timer(duration * 0.111).timeout
+	await get_tree().create_timer(duration * 0.333).timeout
 	visible = false
 	queue_free()
