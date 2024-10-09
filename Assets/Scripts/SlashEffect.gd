31eb418372dca376787abefc3e385110a16f99f6
Jack Caesar <jackachulian@gmail.com>
2024-10-08 19:08:18 -0500

    sword attack 2 slash

--- Assets/Scripts/SlashEffect.gd
+++ Assets/Scripts/SlashEffect.gd
@@ -3,9 +3,11 @@ extends Node3D
 
 @export var duration: float = 0.15
 @export var mesh: CSGPolygon3D
+#@export var is_left_to_right: bool = false
 	
 func animate():
 	visible = true
+	#mesh.material.set_shader_parameter("is_left_to_right", is_left_to_right)
 	mesh.material.set_shader_parameter("t", 0)
 	await get_tree().create_timer(duration * 0.666).timeout
 	mesh.material.set_shader_parameter("t", 0.5)
