700055cc7204757abf6c03db0d55aba6c133eade
Jack Caesar <jackachulian@gmail.com>
2024-10-31 16:08:01 -0500

    Fireball burst projectiles and improved visuals

--- Assets/Scripts/Projectile.gd
+++ Assets/Scripts/Projectile.gd
@@ -20,9 +20,18 @@ extends RigidBody3D
 
 @export var max_snap_speed: float = 2.0
 
+# all children that should last a little bit longer after the projectile is destroyed. (particle effects, etc)
+# they are moved out of the projectile parent upon destroy, and freed after a short delay.
+@export var lingering_effects: Array[Node]
+
 @onready var hitbox: Hitbox = $Hitbox
 
 @onready var ground_snap: RayCast3D = $GroundSnapRayCast3D
+
+@onready var particles: GPUParticles3D = $GPUParticles3D
+@onready var burst_particles: GPUParticles3D = $BurstGPUParticles3D
+@onready var omni_light_3d: OmniLight3D = $OmniLight3D
+
 
 
 var remaining_lifetime: float
@@ -30,15 +39,22 @@ var remaining_lifetime: float
 var base_snap_height: float
 var current_offset: float
 
+# entity that shot this projectile, if any
+var entity: Entity
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	freeze = true
+	#if not entity:
+		#print("clearing proj without an entity that shot it: ", self)
+		#queue_free()
+		#return
 	
 	# Should only be on the Projectiles layer, which will only collide with terrain and objects, not entities.
 	collision_layer = 2
 
 # entity: the entity shooting the projectile.
 func shoot(entity: Entity):
+	self.entity = entity
 	remaining_lifetime = lifetime;
 	freeze = false
 	#linear_velocity = entity.quaternion * shoot_velocity # forward
@@ -65,6 +81,9 @@ func shoot(entity: Entity):
 	if ground_snap:
 		base_snap_height = global_position.y - entity.global_position.y
 		ground_snap.target_position = (base_snap_height + max_snap_distance) * Vector3.DOWN
+		
+	if particles:
+		particles.emitting = true
 	
 		
 func on_hitbox_deal_damage(area: Area3D):
@@ -94,6 +113,29 @@ func on_collide(body: Node):
 	if destroy_on_hit_wall:
 		destroy()
 
-func destroy():
+# true if the projectile hit something and bursted. False if it despawned at the end of its lifetime.
+var destroyed: bool = false
+func destroy(collided: bool = true):
+	if destroyed:
+		return
+	destroyed = true
+	
+	if particles:
+		particles.emitting = false
+	if burst_particles:
+		burst_particles.emitting = true
+	
+	for node in lingering_effects:
+		node.reparent(entity.get_parent() if entity else Entity.player.get_parent())
 	queue_free()
+	
+	if omni_light_3d:
+		var anim_dur := burst_particles.lifetime*0.667 if burst_particles else 0.33
+		get_tree().create_tween().tween_property(omni_light_3d, "light_energy", 0, anim_dur)
+		get_tree().create_tween().tween_property(omni_light_3d, "omni_range", omni_light_3d.omni_range*1.5, anim_dur)
+		
+	await get_tree().create_timer(2.0).timeout
+	
+	for node in lingering_effects:
+		node.queue_free()
 	return
